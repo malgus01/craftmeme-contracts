1fd507595228aa195f43edbc7efbfec213ccd908
Riiz0 <shawnrizo@hotmail.com>
2024-10-22 12:41:56 -0400

    Feat: Configure VestingContract.sol

--- src/VestingContract.sol
+++ src/VestingContract.sol
@@ -1,4 +1,108 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract VestingContract {}
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title TokenVesting
+ * @author CraftMeme
+ * @dev A token vesting contract that will release tokens over time to beneficiaries.
+ */
+contract VestingContract is Ownable {
+    error VestingAlreadySet();
+    error NoVestingSchedule();
+    error VestingIsRevoked();
+    error NoTokensAreDue();
+    error AlreadyRevoked();
+
+    using SafeERC20 for IERC20;
+
+    IERC20 private immutable memeToken;
+
+    struct VestingSchedule {
+        uint256 start;
+        uint256 duration;
+        uint256 amount;
+        uint256 released;
+        bool revoked;
+    }
+
+    mapping(address => VestingSchedule) private vestingSchedules;
+
+    event TokensReleased(address indexed beneficiary, uint256 indexed amount);
+    event VestingRevoked(address indexed beneficiary);
+
+    constructor(address InitialOwner, IERC20 _memeToken) Ownable(InitialOwner) {
+        memeToken = _memeToken;
+    }
+
+    /**
+     * @dev Sets up a vesting schedule for a beneficiary.
+     * @param beneficiary Address of the beneficiary.
+     * @param start Vesting start time (in UNIX timestamp).
+     * @param duration Duration of the vesting period in seconds.
+     * @param amount Total number of tokens to be vested.
+     */
+    function setVestingSchedule(
+        address beneficiary,
+        uint256 start,
+        uint256 duration,
+        uint256 amount
+    )
+        external
+        onlyOwner
+    {
+        require(vestingSchedules[beneficiary].amount == 0, VestingAlreadySet());
+
+        vestingSchedules[beneficiary] =
+            VestingSchedule({ start: start, duration: duration, amount: amount, released: 0, revoked: false });
+    }
+
+    /**
+     * @dev Releases vested tokens to the beneficiary.
+     * @param beneficiary The address receiving the vested tokens.
+     */
+    function release(address beneficiary) public {
+        VestingSchedule storage schedule = vestingSchedules[beneficiary];
+        require(schedule.amount > 0, NoVestingSchedule());
+        require(!schedule.revoked, VestingIsRevoked());
+
+        uint256 unreleased = vestedAmount(beneficiary) - schedule.released;
+        require(unreleased > 0, NoTokensAreDue());
+
+        schedule.released += unreleased;
+        memeToken.safeTransfer(beneficiary, unreleased);
+
+        emit TokensReleased(beneficiary, unreleased);
+    }
+
+    /**
+     * @dev Revokes the vesting schedule for a beneficiary.
+     * @param beneficiary The address whose vesting schedule is being revoked.
+     */
+    function revoke(address beneficiary) external onlyOwner {
+        VestingSchedule storage schedule = vestingSchedules[beneficiary];
+        require(!schedule.revoked, AlreadyRevoked());
+
+        schedule.revoked = true;
+        emit VestingRevoked(beneficiary);
+    }
+
+    /**
+     * @dev Returns the amount of tokens that have vested for a beneficiary.
+     * @param beneficiary The address of the beneficiary.
+     * @return The amount of tokens vested.
+     */
+    function vestedAmount(address beneficiary) public view returns (uint256) {
+        VestingSchedule storage schedule = vestingSchedules[beneficiary];
+
+        if (block.timestamp < schedule.start) {
+            return 0;
+        } else if (block.timestamp >= schedule.start + schedule.duration) {
+            return schedule.amount;
+        } else {
+            return (schedule.amount * (block.timestamp - schedule.start)) / schedule.duration;
+        }
+    }
+}
