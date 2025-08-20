9f3f950ecaa54287ac1c45a02e20fc0bef8b613a
hectiik <mlghectiik@gmail.com>
2025-08-20 10:38:35 -0400

    Feat: Add Necessary Imports

--- src/FactoryTokenContractV2.sol
+++ src/FactoryTokenContractV2.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { TokenContract } from "./helpers/TokenContract.sol";
+import { MultiSigContract } from "./MultiSigContract.sol";
+import { LiquidityManager } from "./LiquidityManager.sol";
+import { VestingContract } from "./VestingContract.sol";
+
 contract FactoryTokenContractV2 {}
