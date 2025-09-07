6d7a80424b418a780627e8479e3e2c7f75e260ef
hectiik <mlghectiik@gmail.com>
2025-09-07 18:56:56 -0400

    Feat: Add Necessary Import Dependencies

--- src/LiquidityManagerV2.sol
+++ src/LiquidityManagerV2.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+import { IPoolManager } from "v4-core/src/interfaces/IPoolManager.sol";
+import { IHooks } from "v4-core/src/interfaces/IHooks.sol";
+import { PoolKey } from "v4-core/src/types/PoolKey.sol";
+import { CurrencyLibrary, Currency } from "v4-core/src/types/Currency.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
+import { VestingContract } from "./VestingContract.sol";
+
 contract LiquidityManagerV2 {}
