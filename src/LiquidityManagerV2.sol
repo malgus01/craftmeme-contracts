51f08b9c12394195eef0029428580b8b1875b75d
flasharbai-intern <flasharbai@gmail.com>
2025-09-13 19:58:05 -0700

    feat: Add Default liquidity threshold

--- src/LiquidityManagerV2.sol
+++ src/LiquidityManagerV2.sol
@@ -81,6 +81,12 @@ contract LiquidityManagerV2 is Ownable, ReentrancyGuard, Pausable {
     /// @notice Protocol fee recipient
     address public protocolFeeRecipient;
 
+    /// @notice Protocol fee (in basis points, e.g., 100 = 1%)
+    uint256 public protocolFee = 50; // 0.5%
+
+    /// @notice Default liquidity threshold (20 USDC)
+    uint256 public defaultLiquidityThreshold = 20 * 1e6;
+    
     ////////////////////
     // Constructor //
     ////////////////////
