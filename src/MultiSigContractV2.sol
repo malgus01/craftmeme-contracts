6b855261e0fea18542538c0afec992a5001e7d2f
malgus01 <mlghectiik@gmail.com>
2025-10-06 10:20:18 -0700

    feat: Add     mapping(uint256 => address[]) public transactionSigners;

--- src/MultiSigContractV2.sol
+++ src/MultiSigContractV2.sol
@@ -127,6 +127,7 @@ contract MultiSigContractV2 is Ownable, ReentrancyGuard, Pausable {
     ////////////////////
         mapping(uint256 => TransactionData) private transactions;
             mapping(uint256 => mapping(address => bool)) public hasSignedTransaction;
+    mapping(uint256 => address[]) public transactionSigners;
 
     ////////////////////
     // Constructor //
